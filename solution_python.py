349c92c305aaee99825dd19a00d873793a0d8c1a
Mayank Kanoria <mayankkanoria@gmail.com>
2020-07-10 13:29:27 -0400

    Added initial solution - passing tests

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,41 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self._undo_actions = []
+        self._redo_actions = []
 
     def add(self, num: int):
-        pass
+        self.value += num 
+        self._undo_actions.append(num)
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self._undo_actions.append(-num)
 
     def undo(self):
-        pass
+        if not self._undo_actions:
+            return
+        action = self._undo_actions.pop()
+        self._redo_actions.append(action)
+        self.value -= action
 
     def redo(self):
-        pass
+        if not self._redo_actions:
+            return
+        action = self._redo_actions.pop()
+        self._undo_actions.append(action)
+        self.value += action
 
     def bulk_undo(self, steps: int):
-        pass
+        while steps > 0:
+            if not self._undo_actions:
+                return
+            self.undo()
+            steps -= 1
 
     def bulk_redo(self, steps: int):
-        pass
+        while steps > 0:
+            if not self._redo_actions:
+                return
+            self.redo()
+            steps -= 1
