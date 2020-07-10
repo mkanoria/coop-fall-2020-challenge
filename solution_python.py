b680a7824a289828fd1ee19e061562f248a74af0
Mayank Kanoria <mayankkanoria@gmail.com>
2020-07-10 13:49:24 -0400

    Code clean up

--- solution_python.py
+++ solution_python.py
@@ -3,41 +3,39 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
-        self._undo_actions = []
-        self._redo_actions = []
+        self._undo_history = []
+        self._redo_history = []
 
     def add(self, num: int):
         self.value += num 
-        self._undo_actions.append(num)
+        self._undo_history.append(num)
 
     def subtract(self, num: int):
         self.value -= num
-        self._undo_actions.append(-num)
+        self._undo_history.append(-num)
 
     def undo(self):
-        if not self._undo_actions:
+        # Return if nothing to undo
+        if not self._undo_history:
             return
-        action = self._undo_actions.pop()
-        self._redo_actions.append(action)
+        action = self._undo_history.pop()
+        self._redo_history.append(action)
         self.value -= action
 
     def redo(self):
-        if not self._redo_actions:
+        # Return if nothing to redo
+        if not self._redo_history:
             return
-        action = self._redo_actions.pop()
-        self._undo_actions.append(action)
+        action = self._redo_history.pop()
+        self._undo_history.append(action)
         self.value += action
 
     def bulk_undo(self, steps: int):
         while steps > 0:
-            if not self._undo_actions:
-                return
             self.undo()
             steps -= 1
 
     def bulk_redo(self, steps: int):
         while steps > 0:
-            if not self._redo_actions:
-                return
             self.redo()
             steps -= 1
